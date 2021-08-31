648e78f6134b1210e5a3ead4a83ddee4c2cda459
Neshan Parvej <neshanparvej@gmail.com>
2021-08-31 23:57:19 +0600

    Creating Menu

---
+++ src/data/dishes.js
@@ -0,0 +1,167 @@
+const Dishes = [
+    {
+        id: 0,
+        name: 'Sanji special',
+        image: 'assets/images/0sanji.jpg',
+        category: 'Meal',
+        label: 'Hot',
+        price: '199',
+        description: 'The restaurant is called Sanji no Oresama Restaurant, and has more variety of dishes for dinner and a buffet for lunch time.',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Chopper",
+                date: "2020-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Brook",
+                date: "2019-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 3,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Usoop",
+                date: "2020-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 4,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Luffy",
+                date: "2020-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 5,
+                rating: 4,
+                comment: "Not that good",
+                author: "Zoro",
+                date: "2020-10-16T17:57:28.556094Z"
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: 'Special meats',
+        image: 'assets/images/1meats.jpg',
+        category: 'Meal',
+        label: 'Hot',
+        price: '199',
+        description: 'The restaurant is called Sanji no Oresama Restaurant, and has more variety of dishes for dinner and a buffet for lunch time.',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Chopper",
+                date: "2020-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Brook",
+                date: "2019-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 3,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Usoop",
+                date: "2020-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 4,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Luffy",
+                date: "2020-10-16T17:57:28.556094Z"
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'Risotto',
+        image: 'assets/images/2risotto.jpg',
+        category: 'Meal',
+        label: 'Hot',
+        price: '199',
+        description: 'The restaurant is called Sanji no Oresama Restaurant, and has more variety of dishes for dinner and a buffet for lunch time.',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Chopper",
+                date: "2020-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Brook",
+                date: "2019-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 3,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Usoop",
+                date: "2020-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 4,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Luffy",
+                date: "2020-10-16T17:57:28.556094Z"
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'Omurice',
+        image: 'assets/images/3omurice.jpg',
+        category: 'Meal',
+        label: 'Hot',
+        price: '199',
+        description: 'The restaurant is called Sanji no Oresama Restaurant, and has more variety of dishes for dinner and a buffet for lunch time.',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Chopper",
+                date: "2020-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Brook",
+                date: "2019-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 3,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Usoop",
+                date: "2020-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 4,
+                rating: 5,
+                comment: "Delicious!",
+                author: "Luffy",
+                date: "2020-10-16T17:57:28.556094Z"
+            }
+        ]
+    },
+]
+
+export default Dishes;
